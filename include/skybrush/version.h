4c786a35b0b52c3b99c55425e44ba3291715e77b
Tamas Nepusz <ntamas@gmail.com>
2024-11-22 13:33:44 +0100

    chore: bumped version to 2.0.0

--- include/skybrush/version.h
+++ include/skybrush/version.h
@@ -29,19 +29,19 @@
  * \def SKYBRUSH_VERSION_MAJOR
  * Major version number of \c libskybrush
  */
-#define SKYBRUSH_VERSION_MAJOR 1
+#define SKYBRUSH_VERSION_MAJOR 2
 
 /**
  * \def SKYBRUSH_VERSION_MINOR
  * Minor version number of \c libskybrush
  */
-#define SKYBRUSH_VERSION_MINOR 9
+#define SKYBRUSH_VERSION_MINOR 0
 
 /**
  * \def SKYBRUSH_VERSION_PATCH
  * Patch level of \c libskybrush
  */
-#define SKYBRUSH_VERSION_PATCH 1
+#define SKYBRUSH_VERSION_PATCH 0
 
 /**
  * \def SKYBRUSH_VERSION
